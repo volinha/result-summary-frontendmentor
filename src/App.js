c7b8be1c9f75f842e26e7249b51062ba3910d388
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2023-03-16 17:53:54 -0300

    add: json data to score and category

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import data from "./data.json";
 
 import FinalResult from "./components/FinalResult";
 import Summary from "./components/Summary";
+import { useState } from "react";
 
 function App() {
+
+  const [myData, setMyData] = useState(data);
+
   return (
     <div className="sm:h-screen flex flex-col sm:flex-row items-center justify-center font-sans text-[18px]">
       <div className="sm:flex sm:shadow-2xl">
-        <FinalResult className="flex-1" />
-        <Summary className="flex-1" />
+        <FinalResult />
+        <Summary rawData={myData}/>
       </div>
     </div>
   );
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -5,19 +5,20 @@ import memory from "../assets/images/icon-memory.svg";
 import verbal from "../assets/images/icon-verbal.svg";
 import visual from "../assets/images/icon-visual.svg";
 
-const Summary = () => {
+const Summary = (props) => {
   return (
     <div className="container min-w-screen sm:max-w-sm">
-      <div className="p-5 mx-6 font-bold">Summary</div>
 
+      <div className="p-5 mx-6 font-bold">Summary</div>
+      
       <div className="bg-lightred/10 mx-10 p-3 rounded-xl">
         <div className="flex justify-between">
           <div className="flex flex-row font-bold text-lightred">
             <img className="mx-2" src={reaction} alt="Reaction" />
-            Reaction
+            {props.rawData[0].category}
           </div>
           <div className="flex flex-row font-bold">
-            <div className="text-gray-700 mx-1">80</div> <div className="text-gray-400">/ 100</div>
+            <div className="text-gray-700 mx-1">{props.rawData[0].score}</div> <div className="text-gray-400">/ 100</div>
           </div>
         </div>
       </div>
@@ -26,10 +27,10 @@ const Summary = () => {
         <div className="flex justify-between">
           <div className="flex flex-row font-bold text-orangeyyellow">
             <img className="mx-2" src={memory} alt="Memory" />
-            Memory
+            {props.rawData[1].category}
           </div>
           <div className="flex flex-row font-bold">
-            <div className="text-gray-700 mx-1">80</div>
+            <div className="text-gray-700 mx-1">{props.rawData[1].score}</div>
             <div className="text-gray-400">/ 100</div>
           </div>
         </div>
@@ -39,10 +40,10 @@ const Summary = () => {
         <div className="flex justify-between">
           <div className="flex flex-row font-bold text-greenteal">
             <img className="mx-2" src={verbal} alt="Verbal" />
-            Verbal
+            {props.rawData[2].category}
           </div>
           <div className="flex flex-row font-bold">
-            <div className="text-gray-700 mx-1">80</div> <div className="text-gray-400">/ 100</div>
+            <div className="text-gray-700 mx-1">{props.rawData[2].score}</div> <div className="text-gray-400">/ 100</div>
           </div>
         </div>
       </div>
@@ -51,16 +52,18 @@ const Summary = () => {
         <div className="flex justify-between">
           <div className="flex flex-row font-bold text-cobaltblue">
             <img className="mx-2" src={visual} alt="Visual" />
-            Visual
+            {props.rawData[3].category}
           </div>
           <div className="flex flex-row font-bold">
-            <div className="text-gray-700 mx-1">80</div> <div className="text-gray-400">/ 100</div>
+            <div className="text-gray-700 mx-1">{props.rawData[3].score}</div> <div className="text-gray-400">/ 100</div>
           </div>
         </div>
       </div>
 
       <div className="flex flex-col items-center mx-10 ">
-        <button className="font-medium w-full py-3 bg-darkgreyblue rounded-3xl text-white">
+        <button className="font-medium w-full py-3 bg-darkgreyblue rounded-3xl text-white"
+          onClick={() => console.log(props.rawData[0].score)}
+        >
           Continue
         </button>
       </div>
